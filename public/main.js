95e81376c937fec4e510984c398c5a285b10a7d3
TannerLoren <77933129+TannerLoren@users.noreply.github.com>
2022-04-29 17:45:38 -0400

    flip coin funct

--- public/main.js
+++ public/main.js
@@ -24,7 +24,17 @@ document.getElementById('guessnav').onclick = function() {
     document.getElementById('guess').className = 'active'
 }
 // Flip one coin and show coin image to match result when button clicked
-
+function flipCoin() {
+    fetch('http://localhost:5000/app/flip/')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            console.log(result);
+            document.getElementById("outcome").innerHTML = result.flip;
+            document.getElementById("image").setAttribute("src", "assets/img/" + result.flip + ".png");
+        })
+}
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
 
