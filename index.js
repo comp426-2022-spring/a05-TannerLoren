2cb23f7ba554d67415912ab6016bec1bc5e46659
TannerLoren <77933129+TannerLoren@users.noreply.github.com>
2022-04-28 23:06:08 -0400

    update

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ if (args['help']) {
     console.log(`index.js [options]
     --port, -p	Set the port number for the server to listen on. Must be an integer
     between 1 and 65535. Defaults to 5000.
+
+    --debug, -d If set to true, creates endlpoints /app/log/access/ which returns
+    a JSON access log from the database and /app/error which throws 
+    an error with the message "Error test successful." Defaults to 
+    false.
