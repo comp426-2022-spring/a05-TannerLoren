1e9f20eace6f942fa83117ea99d0c0197ef90ec0
TannerLoren <77933129+TannerLoren@users.noreply.github.com>
2022-04-29 17:44:29 -0400

    single nav

--- public/main.js
+++ public/main.js
@@ -5,7 +5,12 @@ document.getElementById('homenav').onclick = function() {
     document.getElementById('multi').className = 'hidden'
     document.getElementById('guess').className = 'hidden'
 }
-
+document.getElementById('singlenav').onclick = function() {
+    document.getElementById('home').className = 'hidden'
+    document.getElementById('single').className = 'active'
+    document.getElementById('multi').className = 'hidden'
+    document.getElementById('guess').className = 'hidden'
+}
 // Flip one coin and show coin image to match result when button clicked
 
 // Flip multiple coins and show coin images in table as well as summary results
